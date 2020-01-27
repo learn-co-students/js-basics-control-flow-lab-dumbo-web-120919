76f98162743a286c87a13f479821a2c844595b1d
teapuddles <kingsley.gleeson@gmail.com>
2020-01-26 20:10:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!";
+  }else if (1999 < feet && feet < 2500){
+    return "I will gladly take your thirty bucks.";
+  }else {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+      case 'not as generous':
+        return "Thank you.";
+        break;
+        default: 
+        return "Bye.";
+  }
 }
