6aa6e900f1a79f919a934e574238a7fe8affe033
angelopoole <angelo.poole@outlook.com>
2020-01-26 23:38:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  let ridePrice;
+  if (feet <= 199) {
+    ridePrice = "This one is on me!"
+  } else if (feet >= 200 && feet <= 2499){
+    ridePrice = "I will gladly take your thirty bucks."
+  } else {
+    ridePrice = "No can do."
+  }
+  return ridePrice;
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  let choice;
+  if (city === "NYC") {
+    choice = 'Ok, sounds good.'
+  } else {
+    choice = "No go."
+  }
+  return choice;
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  let generosity;
+  if (tip === "generous") {
+    generosity = "Thank you so much."
+  } else if (tip === "not as generous") {
+    generosity = "Thank you."
+  } else {
+    generosity = "Bye."
+  }
+  return generosity;
   // Write your code here!
 }
