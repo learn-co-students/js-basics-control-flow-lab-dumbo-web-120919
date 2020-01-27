6bbd3265a588a242545386415e085a03c0c52593
David Cha <davidcha17@yahoo.com>
2020-01-27 15:55:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someFeet){
+
+  let result 
+  if (someFeet <= 400 ) {
+    result = "This one is on me!"
+
+  } else if (someFeet > 2500) {
+    result = "No can do."
+  
+  } else if (someFeet > 2000) {
+    result = "I will gladly take your thirty bucks."    
+  }
+
+  return result 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(place){
+  
+   return place === "NYC" ? place = "Ok, sounds good." : place = "No go.";
+  // if (passenger == "NYC") {
+  //   something = "Ok, sounds good."
+  // }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+
+    case 'generous':
+      return "Thank you so much."
+      break;
+
+    case 'not as generous':
+      return "Thank you."
+      break;
+
+    default:
+        return 'Bye.' 
+
+  }
 }
